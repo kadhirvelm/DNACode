d2d22ac15171c50a13cdd05eb1a0e96fb8582337
Kadhir Manickam <kadhirvelm@berkeley.edu>
2015-12-13 16:50:04 -0800

    Crispi variables updated

--- CRISPR:Cas9 Sites/Crispi.py
+++ CRISPR:Cas9 Sites/Crispi.py
@@ -1,3 +1,4 @@
+##! /usr/bin/python
 from __future__ import division
 import sys
 import random
@@ -11,17 +12,19 @@ def main():
     print("Loading fasta file...")
     mySeq, name = loadFasta()  # load the fasta file
     print("Fasta file loaded.")
+    maxMismatches, selectionSize, numberOutputted, cas9Site = loadParameters()
     print("Locating and parsing NGG sites...")
-    myDict, indices = parseFasta(mySeq, size)  # parse the fasta file for .{21}GG (regex)
+    myDict, indices = parseFasta(mySeq, size, cas9Site, selectionSize)  # parse the fasta file for .{21}GG (regex)
     print("NGG Sites found.")
     print("Calculating off-targets...")
-    myDict = callOnParsers(myDict, mySeq)
+    myDict = callOnParsers(myDict, mySeq, maxMismatches)
     print("Calculated off-targets.")
     print("Writing output file...")
-    writeFile(myDict, indices, name)
+    writeFile(myDict, indices, name, mySeq, str(maxMismatches), str(selectionSize), str(numberOutputted), cas9Site)
     print("Finished. Check output.txt")
 
 
+# Will open and save the fasta file along with the name
 def loadFasta():
     # Opening the input file, with exception handling.
     try:
@@ -31,7 +34,7 @@ def loadFasta():
         for seq in SeqIO.parse(inFile, "fasta"):
             fullSeq += seq.seq
     except IndexError:
-        print("You must specify a fasta filename to run and an output name.")
+        print("Usage: python3 Crispi.py Input.fasta OutputName MaxMismatches SelectionSize NumberOutputted [NGG/NAG]")
         sys.exit()
     except FileNotFoundError:
         print("Can't find " + sys.argv[1])
@@ -39,17 +42,50 @@ def loadFasta():
     return str(fullSeq), name
 
 
-def parseFasta(mySeq, size):
+# Loads the parameters from terminal for the max mismatches, the selection size,
+# the number outputted and the Cas9 sites
+def loadParameters():
+    try:
+        maxMismatches = sys.argv[3]
+        if maxMismatches == '-':
+            maxMismatches = 3
+        selectionSize = sys.argv[4]
+        if selectionSize == '-':
+            selectionSize = 50
+        numberOutputted = sys.argv[5]
+        if numberOutputted == '-':
+            numberOutputted = 10
+        cas9Site = sys.argv[6]
+        if cas9Site == '-':
+            cas9Site = 'NGG'
+    except IndexError:
+        print("Usage: python3 Crispi.py Input.fasta OutputName MaxMismatches SelectionSize NumberOutputted [NGG/NAG]")
+        sys.exit()
+    return int(maxMismatches), int(selectionSize), int(numberOutputted), cas9Site
+
+
+# Given the sequence and total size, this will parse through the given file, returning 50 random
+# Cas9 NGG sites, along with their indices
+def parseFasta(mySeq, size, cas9Site, selectionSize):
     indices = {}
     matchDict = {}
     newMatchDict = {}
     currIndex = []
-    pattern = ".{" + str(size) + "}GG"
+    if cas9Site == 'NGG':
+        pattern = ".{" + str(size) + "}GG"
+    elif cas9Site == 'NAG':
+        pattern = ".{" + str(size) + "}AG"
+    else:
+        print("Neither NGG or NAG selected")
+        sys.exit()
     m = regex.findall(pattern, mySeq)
     for kmer in regex.finditer(pattern, mySeq):
         matchDict[kmer.group(0)] = 0
         indices[kmer.group(0)] = str(kmer.start()) + " " + str(kmer.end())
-    for x in range(0, 50):
+    if selectionSize > len(matchDict):
+        print("Selection size is greater than the total number of cas9 sites available.")
+        sys.exit()
+    for x in range(0, selectionSize):
         index = random.randint(x, len(m) - 1)
         if index in currIndex:
             index = x
@@ -60,6 +96,9 @@ def parseFasta(mySeq, size):
     return newMatchDict, indices
 
 
+# Given the sequence, dictionary with NGG sites, and a particular NGG site, will check for exact matches
+# If it finds any, it will delete the NGG site out of the dictionary, since it's not worth checking anymore
+# Then it returns the dictionary
 def parseOffTarget(mySeq, myDict, kmer):
     total = (len(regex.findall(kmer, mySeq)) - 1) * 2  # arbitrary
     if total > 10:
@@ -69,6 +108,7 @@ def parseOffTarget(mySeq, myDict, kmer):
     return myDict
 
 
+# Will check for 1 off target mismatch
 def parseOffTarget1(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     total = 0
     for bp in range(start, end):
@@ -81,6 +121,7 @@ def parseOffTarget1(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     return myDict
 
 
+# Will check for 2 off target mismatches
 def parseOffTarget2(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     for bp in range(start, end - 1):
         temp2 = temp + helperRes(start, bp, kmer)
@@ -88,6 +129,7 @@ def parseOffTarget2(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     return myDict
 
 
+# Will check for 3 off target mismatches
 def parseOffTarget3(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     for bp in range(start, end - 2):
         temp1 = temp + helperRes(start, bp, kmer)
@@ -95,6 +137,7 @@ def parseOffTarget3(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     return myDict
 
 
+# Will check for 4 off target mismatches
 def parseOffTarget4(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     for bp in range(start, end - 2):
         temp1 = temp + helperRes(start, bp, kmer)
@@ -102,6 +145,7 @@ def parseOffTarget4(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     return myDict
 
 
+# Will check for 5 off target mismatches
 def parseOffTarget5(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     for bp in range(start, end - 2):
         temp1 = temp + helperRes(start, bp, kmer)
@@ -109,6 +153,7 @@ def parseOffTarget5(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     return myDict
 
 
+# Will check for 6 off target mismatches
 def parseOffTarget6(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     for bp in range(start, end - 2):
         temp1 = temp + helperRes(start, bp, kmer)
@@ -116,6 +161,7 @@ def parseOffTarget6(mySeq, myDict, start, end, temp, kmer, numOffTargets):
     return myDict
 
 
+# Will check for 7 off target mismatches
 def parseOffTarget7(mySeq, myDict, start, end, kmer, numOffTargets):
     for bp in range(start, end - 2):
         temp = helperRes(start, bp, kmer)
@@ -123,14 +169,19 @@ def parseOffTarget7(mySeq, myDict, start, end, kmer, numOffTargets):
     return myDict
 
 
-def callOnParsers(myDict, mySeq):
+# Given the dictionary full of NGG sites and the total sequence, this will go through and check for up to
+# totalOffStandard mismatches in each of the NGG sites, updating the percentage done as it goes
+def callOnParsers(myDict, mySeq, maxMismatches):
     counter = 0
     print("Number of Cas9 Sites:" + str(len(myDict)))
     print("Status: ")
     myDictCopy = myDict.copy()
+    if maxMismatches > 7:
+        print("Max Mismatches can't be greater than 7.")
+        sys.exit()
     for kmer in myDictCopy:
         totalOff = 0
-        totalOffStandard = 3
+        totalOffStandard = maxMismatches
         while totalOff < totalOffStandard + 1:
             if totalOff == 7:
                 myDict = parseOffTarget7(mySeq, myDict, 0, len(kmer) - 2, kmer, totalOff)
@@ -149,7 +200,7 @@ def callOnParsers(myDict, mySeq):
             elif totalOff == 0:
                 myDict = parseOffTarget(mySeq, myDict, kmer)
             totalOff += 1
-            percent = (1 / (len(myDictCopy) + 1)) * (totalOff) / totalOffStandard
+            percent = (1 / (len(myDictCopy) + 1)) * totalOff / totalOffStandard
             percent = ((counter / len(myDictCopy)) + percent) * 100
             percent = round(percent, 2)
             print(str(percent) + "% ")
@@ -159,14 +210,18 @@ def callOnParsers(myDict, mySeq):
     return myDict
 
 
+# Given a start, an end, and a particular kmer in question, will return a regex expression looking for
+# the site, and replacing the last bp with a mismatch
 def helperRes(start, bp, kmer):
     return kmer[start:bp] + regExDefiner(kmer[bp])
 
 
+# The end of the regular expression specified by helperRes
 def helperResEnd(start, bp, kmer):
     return helperRes(start, bp, kmer) + kmer[bp + 1:]
 
 
+# Given a base pair, will return a regular expression for a mismatch
 def regExDefiner(char):
     if char == 'A':
         return '[T,C,G]'
@@ -178,19 +233,26 @@ def regExDefiner(char):
         return '[A,T,C]'
 
 
-def writeFile(myDict, indicies, name):
+# Writes the output file, given the dictionary, the indices, the name of the file and the sequence
+def writeFile(myDict, indicies, name, mySeq, maxMismatches, selectionSize, numberOutputted, cas9Site):
     counter = 0
     sortDict = sorted(myDict, key = myDict.get)
     fileTemp = open(name + ".txt", 'w+')
     fileTemp.write("Crispi -- Version 66.0123 Beta \n")
     fileTemp.write("Note: \n-->Indicies are exclusive. "
                    + "\n-->The lower the score, the better the site, aka the less off-target probability\n\n")
+    fileTemp.write("-- Parameters --\n")
+    fileTemp.write("Total Sequence Length: " + str(len(mySeq)) + "\n")
+    fileTemp.write("Max Mismatches: " + maxMismatches + ", Selection Size: "
+                   + selectionSize + ", Cas9 Site: " + cas9Site + "\n\n")
     fileTemp.write("Sequence (start, end) Score: --\n")
     for kmer in sortDict:
         counter += 1
         fileTemp.write(str(kmer) + " ( " + indicies[kmer] + " )" + " Score: " + str(myDict[kmer]) + "\n")
-        if counter == 10:
+        if counter == numberOutputted:
             break
+    if counter < numberOutputted:
+        print("Not enough cas9 sites, outputted: " + str(counter))
 
 
 main()
--- README.md
+++ README.md
@@ -1,25 +1,25 @@
-The PySeqSimulator.py file does the following:
+PySeqSimulator
 
 Use '--calc fasta.fasta' to create a random sequence based on fasta.fasta
 Or Use '--load params' to create a random sequence based on the parameters in params
 Follow either command above with '--save file' to save the logistics data to file.
 
-The ReverseCompliment file does the following:
+ReverseCompliment
 
 Run it with a fasta file and it'll write the reverse compliment to the terminal.
 
-The InformationContent file does the following:
+InformationContent
 
 Run it with a Stockholm formatted file as its input and it'll spit out the top ten 
 lowest Shannon entropy columns, and the top fifty mutual information columns.
 
-The Pairwise Alignment file does the following:
+Pairwise Alignment:
 
 Run it with a similarity matrix as the first argument and a sequence file as the second
 to get the first two sequences inside the sequence file aligned and printed to the 
 terminal.
 
-Crispi.py file does the following:
+Crispi:
 
 Run it with any fasta file and it'll create an output.txt with the best CRISPR/Cas9 NGG sites
 present in the file.'
